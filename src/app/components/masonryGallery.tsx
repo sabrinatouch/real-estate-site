6295490f31400affa06b0c533aff3820eadd49c3
sabrinatouch <sabrinajindatouch@gmail.com>
2024-10-14 19:46:28 -0600

    Lightbox button functionality

--- src/app/components/masonryGallery.tsx
+++ src/app/components/masonryGallery.tsx
@@ -27,10 +27,12 @@ export function MasonryGallery() {
 
     const [lightboxDisplay, setLightboxDisplay] = useState(false);
     const [imageToShow, setImageToShow] = useState('');
+    const [imageToShowIndex, setImageToShowIndex] = useState(0);
 
-    const showImage = (imageLink : string) => {
+    const showImage = (imageLink : string, index : number) => {
         // set imageToShow to be the one that's been clicked on
         setImageToShow(imageLink);
+        setImageToShowIndex(index);
 
         // set lightbox visibility to true
         setLightboxDisplay(true);
@@ -40,8 +42,32 @@ export function MasonryGallery() {
         setLightboxDisplay(false)
     }
 
+    const showPrev = (event: SyntheticEvent) => {
+        event.stopPropagation()
+        let currentIndex = imageToShowIndex;
+        let prevIndex = currentIndex - 1;
+
+        if (prevIndex < 0) {
+            prevIndex = images.length - 1
+        }
+        
+        let prevImage = images[prevIndex]
+        setImageToShow(prevImage.imageLink);
+        setImageToShowIndex(prevIndex);
+    }
+
     const showNext = (event: SyntheticEvent) => {
         event.stopPropagation()
+        let currentIndex = imageToShowIndex;
+        let nextIndex = currentIndex + 1;
+        
+        if (nextIndex >= images.length) {
+            nextIndex = 0
+        }
+
+        let nextImage = images[nextIndex]
+        setImageToShow(nextImage.imageLink);
+        setImageToShowIndex(nextIndex);
     }
 
     const theme = useTheme();
@@ -51,7 +77,7 @@ export function MasonryGallery() {
         <div>
             <ImageList variant="masonry" cols={matchDownMd ? 1 : 2} gap={8}>
                 {images.map((image, index) => (
-                    <ImageListItem key={index} onClick={() => showImage(image.imageLink)}>
+                    <ImageListItem key={index} onClick={() => showImage(image.imageLink, index)}>
                         <img 
                             src={image.imageLink}
                             alt=""
@@ -62,10 +88,10 @@ export function MasonryGallery() {
                 ))}
             </ImageList>
             { lightboxDisplay ? 
-                <div id="lightbox" onClick={hideLightbox} className="flex items-center justify-content-between fixed top-0 left-0 z-20 w-full h-full">
-                    <a className="btn">❮</a>
+                <div id="lightbox" onClick={hideLightbox} className="flex items-center justify-content-between fixed top-0 left-0 z-20 w-3/4 h-full">
+                    <a className="btn" onClick={showPrev}>❮</a>
                     <img id="lightbox-img" src={imageToShow}></img>
-                    <a className="btn">❯</a>
+                    <a className="btn" onClick={showNext}>❯</a>
                 </div>
             : '' }
         </div>
